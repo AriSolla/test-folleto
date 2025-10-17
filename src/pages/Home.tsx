5e26cffe2c9065d6505ddd74aef24bc213380699
AriSolla <arielsolla@gmail.com>
2025-10-17 14:42:37 -0300

    v2: cambia color de fondo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -98,7 +98,7 @@ export default function Home() {
   };
 
   return (
-    <Box sx={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
+    <Box sx={{display: "flex", flexDirection: "column", minHeight: "100vh", background:'red'}}>
 
       <HomeNavbar
         search={searchTerm}
